689efeb25a8ea172f9c6c1f8dd498c54b2b2d175
MetaFight <MetaFight@gmail.com>
2016-08-21 22:54:09 +0100

    Fixed splashscreen font on UWP client.  Added some Hacko Pastorius.

--- OxHack.Inventory.MobileClient/OxHack.Inventory.MobileClient/Views/ItemDetailsPage.xaml.cs
+++ OxHack.Inventory.MobileClient/OxHack.Inventory.MobileClient/Views/ItemDetailsPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OxHack.Inventory.MobileClient.ViewModels;
 using Xamarin.Forms;
 using System.ComponentModel;
+using Prism.Commands;
 
 namespace OxHack.Inventory.MobileClient.Views
 {
@@ -15,6 +16,26 @@ namespace OxHack.Inventory.MobileClient.Views
 		{
 			this.InitializeComponent();
 			this.BindingContext = viewModel;
+
+			// Hacko Pastorius!
+			foreach (var photo in viewModel.Photos)
+			{
+				var image = new Image()
+				{
+					Source = photo,
+				};
+
+				var command = new DelegateCommand(() => Device.OpenUri(photo));
+
+				image.GestureRecognizers.Add(
+					new TapGestureRecognizer()
+					{
+						NumberOfTapsRequired = 2,
+						Command = command
+					});
+
+				this.photoLayout.Children.Add(image);
+			}
 		}
 	}
 }
---
+++ OxHack.Inventory.MobileClient/OxHack.Inventory.MobileClient/Views/ToggleView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms;
+
+namespace OxHack.Inventory.MobileClient.Views
+{
+    public partial class ToggleView : StackLayout
+    {
+		public ToggleView()
+		{
+			this.InitializeComponent();
+		}
+
+		public string ToggledTrueText
+		{
+			get
+			{
+				return (string)this.GetValue(ToggleView.ToggledTrueTextProperty);
+			}
+			set
+			{
+				this.SetValue(ToggleView.ToggledTrueTextProperty, value);
+			}
+		}
+
+		public static readonly BindableProperty ToggledTrueTextProperty =
+			BindableProperty.Create(nameof(ToggleView.ToggledTrueText), typeof(string), typeof(ToggleView), true.ToString());
+
+		public string ToggledFalseText
+		{
+			get
+			{
+				return (string)this.GetValue(ToggleView.ToggledFalseTextProperty);
+			}
+			set
+			{
+				this.SetValue(ToggleView.ToggledFalseTextProperty, value);
+			}
+		}
+
+		public static readonly BindableProperty ToggledFalseTextProperty =
+			BindableProperty.Create(nameof(ToggleView.ToggledFalseText), typeof(string), typeof(ToggleView), false.ToString());
+	}
+}
